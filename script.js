472037511b47b88712e93c5c39b5bcccbfd71e75
Justin Bender <jrkbender@gmail.com>
2018-01-01 16:22:14 -0500

    looped through adding the sums together

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function addTwoDigits(n) {
   //create an output array
-    output = [];
+    var sum = 0;
   //make number into string
     var nums = n.toString();
   //loop through string and add each digit to the array
     for(var i = 0; i < nums.length; i++) {
-        output.push(+nums.charAt(i));
+        sum += parseInt(nums[i]);
     }
 //add array num 0 and array num 1
-    return output[0] + output[1];
+    return sum;
 
 }
 
