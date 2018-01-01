f0aa342f3ec1bad1059ebc3a4a68dbfeeb04f817
Justin Bender <jrkbender@gmail.com>
2018-01-01 16:28:29 -0500

    looped through string to add to sum

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function addTwoDigits(n) {
     var sum = 0;
   //make number into string
     var nums = n.toString();
-  //loop through string and add each digit to the array
-    for(var i = 0; i < nums.length; i++) {
-        sum += parseInt(nums[i]);
-    }
+  //loop through string and add each digit to the sum
+  for(var i = 0; i < nums.length; i++) {
+      sum += parseInt(nums.charAt(i));
+  }
 //add array num 0 and array num 1
     return sum;
 
@@ -17,3 +17,8 @@ console.log(addTwoDigits(n));
 
 n = 48;
 console.log(addTwoDigits(n));
+
+
+n = 11112345;
+console.log(addTwoDigits(n));
+// 1 + 1 + 1 + 1 + 2 + 3 + 4 + 5 = 18
